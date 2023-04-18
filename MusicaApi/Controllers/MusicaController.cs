0d5469bb175f8ffcedb43679d0075c60496b3505
felix <felix@DESKTOP-FSGMO7O>
2023-04-18 17:51:42 -0600

    Api

--- MusicaApi/Controllers/MusicaController.cs
+++ MusicaApi/Controllers/MusicaController.cs
@@ -18,19 +18,96 @@ namespace MusicaApi.Controllers
             cancionesRepository = new Repository<Canciones>(context);
         }
 
-        [HttpGet]
+        [HttpGet("buscar")]
         public IActionResult Get()
         {
             var canciones = cancionesRepository.GetAll();
             return Ok(canciones);
         }
 
-
-        [HttpPost]
+        [HttpPost("insertar")]
         public IActionResult Post(Canciones c)
         {
-            return Ok();
+            if (c == null)
+            {
+                return BadRequest("La canción.");
+            }
+
+            if (string.IsNullOrWhiteSpace(c.Autor))
+            {
+                return BadRequest("Especifíque el autor.");
+            }
+
+            if (string.IsNullOrWhiteSpace(c.Titulo))
+            {
+                return BadRequest("Especifíque el titulo.");
+            }
+
+            if (string.IsNullOrWhiteSpace(c.Duracion))
+            {
+                return BadRequest("Especifíque la duración.");
+            }
+
+            if (cancionesRepository.GetAll().Any(x => x.Titulo == c.Titulo && x.Autor == c.Autor))
+            {
+                return BadRequest("Esa canción ya existe.");
+            }
+
+            cancionesRepository.Insert(c);
+            return Ok("Insertado correctamente.");
         }
 
+        [HttpPut("actualizar")]
+        public IActionResult Put(Canciones c)
+        {
+            Canciones? canciones = cancionesRepository.Get(c.Id);
+
+            if (canciones == null)
+            {
+                NotFound("No se encontro la canción");
+            }
+            if (string.IsNullOrWhiteSpace(c.Autor))
+            {
+                return BadRequest("Especifíque el autor.");
+            }
+
+            if (string.IsNullOrWhiteSpace(c.Titulo))
+            {
+                return BadRequest("Especifíque el titulo.");
+            }
+
+            if (string.IsNullOrWhiteSpace(c.Duracion))
+            {
+                return BadRequest("Especifíque la duración.");
+            }
+
+            if (cancionesRepository.GetAll().Any(x => x.Titulo == c.Titulo && x.Autor == c.Autor) && c.Id != 0)
+            {
+                return BadRequest("Esa canción ya existe.");
+            }
+
+            if (canciones != null)
+            {
+                canciones.Titulo = c.Titulo;
+                canciones.Autor = c.Autor;
+                canciones.Duracion = c.Duracion;
+                cancionesRepository.Update(canciones);
+            }
+            return Ok("Actualizado correctamente.");
+        }
+
+        [HttpDelete("eliminar/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var cancion = cancionesRepository.Get(id);
+            if (cancion == null)
+            {
+                return NotFound("No se encontro la canción");
+            }
+
+            cancionesRepository.Delete(cancion);
+            return Ok();
+
+        }
     }
 }
--- MusicaApi/Program.cs
+++ MusicaApi/Program.cs
@@ -1,16 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using MusicaApi.Data;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<Sistem21MusicaContext>(x => x.UseMySql("server=sistemas19.com;user=sistem21_musica;password=sistemas19_;database=sistem21_musica", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.17-mariadb")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                          policy.AllowAnyHeader();
+                          policy.AllowAnyMethod();
+                          policy.AllowAnyOrigin();
+                      });
+});
+
 var app = builder.Build();
 
 app.UseRouting();
 
 app.MapControllers();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();

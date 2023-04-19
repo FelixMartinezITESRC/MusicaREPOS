ff28ff30a5eba5a4d448ebfe58c67cea3abbaaef
felix <felix@DESKTOP-FSGMO7O>
2023-04-19 10:09:42 -0600

    Cambios

--- MusicaApi/Program.cs
+++ MusicaApi/Program.cs
@@ -13,8 +13,6 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://example.com",
-                                              "http://www.contoso.com");
                           policy.AllowAnyHeader();
                           policy.AllowAnyMethod();
                           policy.AllowAnyOrigin();
